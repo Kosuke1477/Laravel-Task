0aa53cad9049331be6109a5438086be4cd7e48f9
Kosuke1477 <66466878+Kosuke1477@users.noreply.github.com>
2020-07-18 19:30:19 +0900

    first

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,6 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
     Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
-　　 Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
-    Route::post('profile/create', 'Admin\ProfileController@create')->middleware('auth');
-    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
-    Route::post('profile/edit', 'Admin\ProfileController@update')->middleware('auth');
 });
 
 
@@ -29,6 +25,20 @@ Route::group(['prefix' => 'XXX'], function() {
     Route::get('news/create', 'XXX\AAAController@bbb');
 
 });
+
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::post('profile/create', 'Admin\ProfileController@create')->middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+    Route::post('profile/edit', 'Admin\ProfileController@update')->middleware('auth');
+    Route::get('news', 'Admin\NewsController@index')->middleware('auth');
+    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth'); // 追記
+    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth'); // 追記
+    Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
+});
+
+
 
 
 Auth::routes();
