5d6e78be4aafb81971384782652cd83eed1ae01a
Kosuke1477 <66466878+Kosuke1477@users.noreply.github.com>
2020-07-18 20:41:43 +0900

    first

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -1,10 +1,9 @@
 <?php
-
 namespace App\Http\Controllers\Admin;
-
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profile;
 class ProfileController extends Controller
 {
     public function add()
@@ -12,16 +11,34 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
 
-    public function create()
+    
+    public function create(Request $request)
     {
+      $this->validate($request,Profile::$rules);
+      $profile = new Profile;
+      $form = $request->all();
+
+      unset($form['_token']);
+      // フォームから送信されてきたimageを削除する
+      unset($form['image']);
+      
+      $profile->fill($form);
+      $profile->save();
+      
         return redirect('admin/profile/create');
     }
 
-    public function edit()
+    
+    public function edit(Request $request)
     {
-        return view('admin.profile.edit');
+        $profile = Profile::find($request->id);
+      if (empty($profile)) {
+        abort(404);    
+      }
+        return view('admin.profile.edit', ['profile_form' => $profile]);
     }
 
+    
     public function update()
     {
         return redirect('admin/profile/edit');
