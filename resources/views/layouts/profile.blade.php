76692fd70921fa7888450d0cf2506bb8fb665fc0
Kosuke1477 <66466878+Kosuke1477@users.noreply.github.com>
2020-07-31 23:15:17 +0900

    課題１９

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -43,7 +43,7 @@
                         <ul class="navbar-nav mr-auto">
 
                         </ul>
-                        
+
                         <!-- Right Side Of Navbar -->
                         <ul class="navbar-nav ml-auto">
                        　<!-- Authentication Links -->
